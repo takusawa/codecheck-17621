d49854a2a3c2206519319d270e29ae7055b6b2fe
沢田拓弥 <takusawa@sawadatakumi-no-MacBook-Air.local>
2017-10-21 00:14:31 +0900

    first commit

--- python/src/main.py
+++ python/src/main.py
@@ -1,2 +1,187 @@
-print ("Hello, world!")
+# coding: utf-8
+import sys
+from datetime import datetime as dt
 
+# 日付を取得し、日付に対応した曜日をかえす
+def get_weekday(date):
+	weekday = ["月","火","水","木","金","土","日"]
+	d = dt.strptime(date, "%Y/%m/%d")
+	return weekday[d.weekday()]
+
+def total_work_time(data):
+	#法定内残業時間数
+	Overtime            = 0
+	#法定外残業時間数
+	LegalOvertime       = 0
+	#深夜残業時間数
+	MidnightOvertime    = 0
+	#所定休日労働時間数
+	HolidayWorking      = 0
+	#法定休日労働時間数
+	LegalHolidayWorking = 0
+
+	#法定労働時間
+	legal_work_standard = 8
+	#法律が定める1週間あたりの労働時間
+	week_work = 40
+	#算出対象となる社員の休憩時間
+	rest_time = 1
+	#算出対象となる社員の就業開始時刻
+	work_s = 8
+	#算出対象となる社員の就業終了時刻
+	work_e = 16
+	#算出対象となる社員の実労働時間
+	work_standard = 7
+	#深夜残業の基準
+	late_night = 22
+	#1日の終わり
+	day_change = 24
+	#1週間
+	a_week = 7
+
+	#1週間単位での総労働時間を算出するための変数
+	total_work = 0
+
+	# 最初の月が記録される部分を除外する
+	work_data = data[1:]
+	# 労働日時を記録する変数
+	day_array = []
+
+	for hoge, work_times in enumerate(work_data):
+		work_time = work_times.split(" ")
+		# 曜日の取得
+		day_of_week = get_weekday(work_time[0])
+		# 日付を取得
+		work_day = dt.strptime(work_time[0], "%Y/%m/%d").day
+		day_array.append(work_day)
+		min_day = day_array[0]
+
+		# 1日の労働開始時間、終了時間を格納する
+		array = []
+		# 1日の労働時間を格納する変数
+		work_sum = 0
+		for work in work_time[1:]:
+			work_time = work.split("-")
+			# 仕事の開始時間、終了時間を取得する変数
+			work_start = 0
+			work_stop  = 0
+			for i, each_time in enumerate(work_time):
+				# 仕事の開始時間、終了時間で分ける
+				if(i % 2 == 0):
+					work_start = dt.strptime(each_time, "%H:%M").hour
+				else:
+					# 労働終了時刻が24時を超えていないかチェック。超えていたら整数値に変換する
+					try:
+						work_stop = dt.strptime(each_time, "%H:%M").hour
+					except ValueError:
+						work_stop = int(each_time[0:2])
+
+				# array に格納する時間が24時を超えていないかチェック
+				try:
+					time = dt.strptime(each_time, '%H:%M').hour
+				except ValueError:
+					time = int(each_time[0:2])
+
+				array.append(time)
+			# 就業開始時間と終了時刻から労働時間を算出し、1日の総労働時間に加える
+			work_total = work_stop - work_start
+			work_sum += work_total
+
+		# 1週間単位で総労働時間をリセットする
+		if(work_day - min_day >= a_week):
+			day_array[0] = work_day
+			total_work = 0
+			total_work += work_sum
+		else:
+			total_work += work_sum
+
+		# 取得した配列の先頭と末尾の要素番号を定義
+		array_first = 0
+		array_last  = len(array) - 1
+
+		start  = array[array_first]
+		finish = array[array_last]
+
+		#1週間の労働時間が40時間を超えるか
+		if(total_work > week_work):
+			LegalOvertime += (total_work - week_work)
+		#規定通りでない出社時間、規定通りの退社時間
+		elif(start != work_s and finish == work_e):
+			if(work_sum == legal_work_standard):
+				Overtime += 1
+			elif(work_sum >= legal_work_standard):
+				Overtime += 1
+				LegalOvertime += work_sum - legal_work_standard
+			else:
+				pass
+		#規定通りの出社時間、退社時間
+		elif(start == work_s and finish == work_e):
+			pass
+		#出社時間、退社時間共に規定通りでない
+		elif(start != work_s and finish != work_e):
+			regular_work = start + rest_time + legal_work_standard
+			Overtime = regular_work - work_e
+			LegalOvertime = work_sum - legal_work_standard
+		#規定通りの出社時間、規定通りでない退社時間
+		elif(start == work_s and finish != work_e):
+			# 8時出社17時退社
+			if(work_sum == legal_work_standard):
+				Overtime += 1
+			# 8時出社、16時より前に退社
+			elif(work_sum < legal_work_standard):
+				pass
+			# 17時以降の退社
+			elif(work_sum > legal_work_standard):
+				#労働時間数
+				legal_time = work_e + 1
+				#法定内残業時間数
+				overtime = legal_time - work_e
+
+				# 土曜日の出社
+				if(day_of_week == "土"):
+					if(finish > day_change):
+						MidnightOvertime += (finish - late_night)
+						HolidayWorking += (work_sum - (finish - day_change))
+						LegalHolidayWorking += (finish - day_change)
+					elif(finish > late_night):
+						MidnightOvertime += (finish - late_night)
+						HolidayWorking += work_sum
+					else:
+						HolidayWorking += work_sum
+				# 日曜日の出社
+				elif(day_of_week == "日"):
+					if(finish > day_change):
+						LegalOvertime += (finish - day_change)
+						MidnightOvertime += (finish - late_night)
+						LegalHolidayWorking += (work_sum - (finish - day_change))
+					elif(finish > late_night):
+						MidnightOvertime += (finish - late_night)
+						LegalHolidayWorking += work_sum
+					else:
+						LegalHolidayWorking += work_sum
+				# 金曜日の出社かつ、日またぎ残業
+				elif(day_of_week =="金" and finish > day_change):
+					Overtime += overtime
+					LegalOvertime += (day_change - legal_time)
+					MidnightOvertime += (finish - late_night)
+					HolidayWorking += (finish - day_change)
+				#平日
+				else:
+					if(finish > late_night):
+						Overtime += overtime
+						LegalOvertime += (work_sum - legal_work_standard)
+						MidnightOvertime += (finish - late_night)
+					else:
+						Overtime += overtime
+						LegalOvertime += (work_sum - legal_work_standard)
+	#結果を出力
+	print(Overtime)
+	print(LegalOvertime)
+	print(MidnightOvertime)
+	print(HolidayWorking)
+	print(LegalHolidayWorking)
+
+# データを受け取る
+if __name__ == "__main__":
+	data = sys.stdin.readlines()
+	total_work_time(data)
--- test/test_optional.js
+++ test/test_optional.js
@@ -3,5 +3,127 @@ var expect = require('expect');
 var run = require('./lib.js').run;
 
 describe('Creative Engineer Exam #1 (Optional Tests)', () => {
-	// You may make additional tests here
+	it('Case #11 should be caluculated', () => co(function*() {
+		const input = [
+			'2017/02',
+			'2017/02/01 09:00-12:00 13:00-16:00'
+		];
+		const expected = [
+			'0', '0', '0', '0', '0'
+		];
+		const actual = yield run('./run.sh', [], input.join('\n'));
+		expect(actual).toEqual(expected);
+	}));
+	it('Case #12 should be caluculated', () => co(function*() {
+		const input = [
+			'2017/02',
+			'2017/02/01 07:00-12:00 13:00-16:00'
+		];
+		const expected = [
+			'1', '0', '0', '0', '0'
+		];
+		const actual = yield run('./run.sh', [], input.join('\n'));
+		expect(actual).toEqual(expected);
+	}));
+	it('Case #13 should be caluculated', () => co(function*() {
+		const input = [
+			'2017/02',
+			'2017/02/01 05:00-12:00 13:00-16:00'
+		];
+		const expected = [
+			'1', '2', '0', '0', '0'
+		];
+		const actual = yield run('./run.sh', [], input.join('\n'));
+		expect(actual).toEqual(expected);
+	}));
+	it('Case #14 should be caluculated', () => co(function*() {
+		const input = [
+			'2017/02',
+			'2017/02/01 08:00-12:00 13:00-15:00'
+		];
+		const expected = [
+			'0', '0', '0', '0', '0'
+		];
+		const actual = yield run('./run.sh', [], input.join('\n'));
+		expect(actual).toEqual(expected);
+	}));
+	it('Case #15 should be caluculated', () => co(function*() {
+		const input = [
+			'2017/02',
+			'2017/02/12 08:00-12:00 13:00-26:00'
+		];
+		const expected = [
+			'0', '2', '4', '0', '15'
+		];
+		const actual = yield run('./run.sh', [], input.join('\n'));
+		expect(actual).toEqual(expected);
+	}));
+	it('Case #16 should be caluculated', () => co(function*() {
+		const input1 = [
+			'2017/01',
+			'2017/01/16 08:00-12:00 13:00-18:00',
+			'2017/01/17 08:00-12:00 13:00-18:00',
+			'2017/01/18 08:00-12:00 13:00-18:00',
+			'2017/01/19 08:00-12:00 13:00-17:00',
+			'2017/01/20 08:00-12:00 13:00-21:00'
+		];
+		const expected1 = [
+			'4', '10', '0', '0', '0'
+		];
+		const actual1 = yield run('./run.sh', [], input1.join('\n'));
+		expect(actual1).toEqual(expected1);
+
+		const input2 = [
+			'2017/01',
+			'2017/01/16 08:00-12:00 13:00-18:00',
+			'2017/01/17 08:00-12:00 13:00-18:00',
+			'2017/01/18 08:00-12:00 13:00-18:00',
+			'2017/01/19 08:00-12:00 13:00-17:00',
+			'2017/01/20 08:00-12:00 13:00-21:00',
+			'2017/01/23 08:00-12:00 13:00-18:00',
+			'2017/01/24 08:00-12:00 13:00-18:00'
+		];
+		const expected2 = [
+			'6', '12', '0', '0', '0'
+		];
+		const actual2 = yield run('./run.sh', [], input2.join('\n'));
+		expect(actual2).toEqual(expected2);
+
+		const input3 = [
+			'2017/01',
+			'2017/01/16 08:00-12:00 13:00-18:00',
+			'2017/01/17 08:00-12:00 13:00-18:00',
+			'2017/01/18 08:00-12:00 13:00-18:00',
+			'2017/01/19 08:00-12:00 13:00-17:00',
+			'2017/01/20 08:00-12:00 13:00-21:00',
+			'2017/01/23 08:00-12:00 13:00-18:00',
+			'2017/01/24 08:00-12:00 13:00-18:00',
+			'2017/01/25 08:00-12:00 13:00-18:00',
+			'2017/01/26 08:00-12:00 13:00-17:00'
+		];
+		const expected3 = [
+			'8', '13', '0', '0', '0'
+		];
+		const actual3 = yield run('./run.sh', [], input3.join('\n'));
+		expect(actual3).toEqual(expected3);
+
+		const input4 = [
+			'2017/01',
+			'2017/01/16 08:00-12:00 13:00-18:00',
+			'2017/01/17 08:00-12:00 13:00-18:00',
+			'2017/01/18 08:00-12:00 13:00-18:00',
+			'2017/01/19 08:00-12:00 13:00-17:00',
+			'2017/01/20 08:00-12:00 13:00-21:00',
+			'2017/01/23 08:00-12:00 13:00-18:00',
+			'2017/01/24 08:00-12:00 13:00-18:00',
+			'2017/01/25 08:00-12:00 13:00-18:00',
+			'2017/01/26 08:00-12:00 13:00-17:00',
+			'2017/01/27 08:00-12:00 13:00-21:00'
+		];
+		const expected4 = [
+			'8', '20', '0', '0', '0'
+		];
+		const actual4 = yield run('./run.sh', [], input4.join('\n'));
+		expect(actual4).toEqual(expected4);
+	}));
 });
